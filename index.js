88657e2651a5c05edd9fbf18c0fe9f38d285fcb5
Elder Lira <elderfranca@hotmail.com>
2022-09-28 15:12:21 -0300

    added tratarErro function

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ import chalk from 'chalk'
 import fs from 'fs'
 
 const caminhaDoArquivo = './arquivos/texto.md'
+
+function tratarErro(erro) {
+    throw new Error(erro)
+}
+
 function pegaArquivo(caminhaDoArquivo) {
     const encode = 'utf-8'
     fs.readFile(caminhaDoArquivo, encode, (erro, texto) => {
         if (erro) {
-            console.log(chalk.red(erro))
+            tratarErro(erro)
         }
         console.log(chalk.blue(texto))
     })
