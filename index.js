1a53e2d5c8809faea42cd3e6c309d69aa062121a
Elder Lira <elderfranca@hotmail.com>
2022-10-02 03:31:36 -0300

    created function extrarLinks

--- index.js
+++ index.js
@@ -3,27 +3,30 @@ import fs from 'fs'
 
 const caminhaDoArquivo = './arquivos/texto.md'
 
+function extrairLinks(texto) {
+    const regex = /\[([^[\]]*)\]\((https?:\/\/[^\s]*)\)/gm
+    const conteudo = [...texto.matchAll(regex)]
+    const resultado = conteudo.map(res => (
+        {[res[1]]: res[2]})
+    )
+   
+    return resultado
+}
+
 function tratarErro(erro) {
     throw new Error(chalk.red(erro.code, 'Arquivo não disponivel no diretorio'))
 }
 
-//utilizando a estrutura try/catch e async/await
 async function pegaArquivo(caminhaDoArquivo) {
     try {
         const encode = 'utf-8'
         const texto = await fs.promises.readFile(caminhaDoArquivo, encode)
-        console.log(chalk.green(texto))
+        console.log(extrairLinks(chalk.green(texto)))
     } catch(erro) {
         tratarErro(erro)
+    } finally {
+        console.log(chalk.blue('processo finalizado'))
     }
 }
-
-// function pegaArquivo(caminhaDoArquivo) {
-//     const encode = 'utf-8'
-//     fs.promises
-//         .readFile(caminhaDoArquivo, encode)
-//         .then(texto => console.log(chalk.green(texto)))
-//         .catch(erro => tratarErro(erro))
-// }
 
 pegaArquivo(caminhaDoArquivo)
