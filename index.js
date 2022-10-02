bca0df4e6534972cf6db81ea14baf4dd795ca90e
Elder Lira <elderfranca@hotmail.com>
2022-10-01 21:58:50 -0300

    refactor function with async/await

--- index.js
+++ index.js
@@ -4,15 +4,26 @@ import fs from 'fs'
 const caminhaDoArquivo = './arquivos/texto.md'
 
 function tratarErro(erro) {
-    throw new Error(erro)
+    throw new Error(chalk.red(erro.code, 'Arquivo não disponivel no diretorio'))
 }
 
-function pegaArquivo(caminhaDoArquivo) {
-    const encode = 'utf-8'
-    fs.promises
-        .readFile(caminhaDoArquivo, encode)
-        .then(texto => console.log(chalk.green(texto)))
-        .catch(erro => tratarErro(erro))
+//utilizando a estrutura try/catch e async/await
+async function pegaArquivo(caminhaDoArquivo) {
+    try {
+        const encode = 'utf-8'
+        const texto = await fs.promises.readFile(caminhaDoArquivo, encode)
+        console.log(chalk.green(texto))
+    } catch(erro) {
+        tratarErro(erro)
+    }
 }
+
+// function pegaArquivo(caminhaDoArquivo) {
+//     const encode = 'utf-8'
+//     fs.promises
+//         .readFile(caminhaDoArquivo, encode)
+//         .then(texto => console.log(chalk.green(texto)))
+//         .catch(erro => tratarErro(erro))
+// }
 
 pegaArquivo(caminhaDoArquivo)
