1de05908f122de4c75e5f8a1d47f7b5e094663eb
Elder Lira <elderfranca@hotmail.com>
2022-10-05 10:18:16 -0300

    created function to print information

--- src/cli.js
+++ src/cli.js
@@ -1,21 +1,39 @@
 import pegaArquivo from "./index.js"
 import fs from 'fs'
+import validaLinks from "./validaLinks.js"
 
 const caminho = process.argv
 
+function imprimeLista (valida, listaLinks, identificador = '') {
+    if (valida) {
+        console.log(
+            'lista validada', 
+            validaLinks(listaLinks, identificador)
+        )
+    } else {
+        console.log(
+            'lista de links', 
+            identificador, listaLinks
+        )
+    }
+}
+
 async function processarTexto(argumento) {
     const caminho = argumento[2]
+    const valida = argumento[3] === '--valida'
+
     try {
         if(fs.statSync(caminho).isFile()) {
             const resultado = await pegaArquivo(caminho)
-            console.log(resultado)
+            imprimeLista(valida, resultado)
         } 
     
         if(fs.statSync(caminho).isDirectory()) {
             const arquivos = await fs.promises.readdir(caminho)
             arquivos.forEach(async (arquivo) => {
                 const lista = await pegaArquivo(`${caminho}/${arquivo}`)
-                console.log('Arquivo: ' + arquivo, lista)
+                imprimeLista(valida, lista, arquivo)
+                // console.log('Arquivo: ' + arquivo, lista)
             })
         }
     } catch (erro) {
